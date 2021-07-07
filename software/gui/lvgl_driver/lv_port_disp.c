75ba78a1c50d994ed4223fb56c884245fc78455d
lucas-xm <duo_lin0119@163.com>
2021-07-08 00:08:10 +0800

    ✨ lvgl移植完成

--- software/gui/lvgl_driver/lv_port_disp.c
+++ software/gui/lvgl_driver/lv_port_disp.c
@@ -1,5 +1,5 @@
 /**
- * @file lv_port_disp.c
+ * @file lv_port_disp_templ.c
  *
  */
 
@@ -22,7 +22,6 @@
 /**********************
  *  STATIC PROTOTYPES
  **********************/
-static void disp_init(void);
 
 static void disp_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p);
 #if LV_USE_GPU
@@ -44,18 +43,32 @@ static void gpu_fill(lv_color_t *dest, uint32_t length, lv_color_t color);
 
 void lv_port_disp_init(void)
 {
-    /*-------------------------
-     * Initialize your display
-     * -----------------------*/
-    disp_init();
-
     /*-----------------------------
      * Create a buffer for drawing
      *----------------------------*/
+
+    /* LittlevGL requires a buffer where it draws the objects. The buffer's has to be greater than 1 display row
+     *
+     * There are three buffering configurations:
+     * 1. Create ONE buffer with some rows: 
+     *      LittlevGL will draw the display's content here and writes it to your display
+     * 
+     * 2. Create TWO buffer with some rows: 
+     *      LittlevGL will draw the display's content to a buffer and writes it your display.
+     *      You should use DMA to write the buffer's content to the display.
+     *      It will enable LittlevGL to draw the next part of the screen to the other buffer while
+     *      the data is being sent form the first buffer. It makes rendering and flushing parallel.
+     * 
+     * 3. Create TWO screen-sized buffer: 
+     *      Similar to 2) but the buffer have to be screen sized. When LittlevGL is ready it will give the
+     *      whole frame to display. This way you only need to change the frame buffer's address instead of
+     *      copying the pixels.
+     * */
+
     /* Example for 1) */
-    static lv_disp_buf_t disp_buf_1;
-    static lv_color_t buf1_1[LV_HOR_RES_MAX * 10];                    /*A buffer for 10 rows*/
-    lv_disp_buf_init(&disp_buf_1, buf1_1, NULL, LV_HOR_RES_MAX * 10); /*Initialize the display buffer*/
+    static lv_disp_buf_t disp_buf;
+    static lv_color_t color_buf[LV_HOR_RES_MAX * 10]; //显示缓冲区,静态的 sram
+    lv_disp_buf_init(&disp_buf, color_buf, NULL, LV_HOR_RES_MAX * 10);
 
     /*-----------------------------------
      * Register the display in LittlevGL
@@ -67,7 +80,6 @@ void lv_port_disp_init(void)
     /*Set up the functions to access to your display*/
 
     /*Set the resolution of the display*/
-    //适配多个屏幕
     disp_drv.hor_res = 240;
     disp_drv.ver_res = 320;
 
@@ -75,7 +87,7 @@ void lv_port_disp_init(void)
     disp_drv.flush_cb = disp_flush;
 
     /*Set a display buffer*/
-    disp_drv.buffer = &disp_buf_1;
+    disp_drv.buffer = &disp_buf;
 
 #if LV_USE_GPU
     /*Optionally add functions to access the GPU. (Only in buffered mode, LV_VDB_SIZE != 0)*/
@@ -95,18 +107,26 @@ void lv_port_disp_init(void)
  *   STATIC FUNCTIONS
  **********************/
 
-/* Initialize your display and the required peripherals. */
-static void disp_init(void)
-{
-    /*You code here*/
-}
-
 /* Flush the content of the internal buffer the specific area on the display
  * You can use DMA or any hardware acceleration to do this operation in the background but
  * 'lv_disp_flush_ready()' has to be called when finished. */
 static void disp_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p)
 {
-    LCD_Fill(area->x1, area->y1, area->x2, area->y2, *(unsigned short int *)color_p);
+    /*The most simple case (but also the slowest) to put all pixels to the screen one-by-one*/
+
+    int32_t x;
+    int32_t y;
+    for(y = area->y1; y <= area->y2; y++) {
+        for(x = area->x1; x <= area->x2; x++) {
+            /* Put a pixel to the display. For example: */
+            /* put_px(x, y, *color_p)*/
+            LCD_DrawPoint(x,y,color_p->full);
+            color_p++;
+        }
+    }
+
+    // LCD_Fill(area->x1, area->y1, area->x2, area->y2, *(unsigned short int *)color_p);
+
     /* IMPORTANT!!!
      * Inform the graphics library that you are ready with the flushing*/
     lv_disp_flush_ready(disp_drv);
@@ -120,6 +140,7 @@ static void disp_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_
 static void gpu_blend(lv_disp_drv_t *disp_drv, lv_color_t *dest, const lv_color_t *src, uint32_t length, lv_opa_t opa)
 {
     /*It's an example code which should be done by your GPU*/
+
     uint32_t i;
     for (i = 0; i < length; i++)
     {
@@ -143,6 +164,12 @@ static void gpu_fill_cb(lv_disp_drv_t *disp_drv, lv_color_t *dest_buf, lv_coord_
             dest_buf[x] = color;
         }
         dest_buf += dest_width; /*Go to the next line*/
+    }
+
+    uint32_t i;
+    for (i = 0; i < length; i++)
+    {
+        dest[i] = color;
     }
 }
 
